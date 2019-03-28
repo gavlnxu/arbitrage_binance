85732bd8dd4e2856761be0f4749bce43be2e4a0f
gainv <gvinaxu@bcanalyst.com>
2019-03-28 11:17:35 +0800

    代码命名

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"github.com/adshao/go-binance"
 	"context"
 	"fmt"
+	"github.com/adshao/go-binance"
+	"math"
 	"strconv"
 	"time"
-	"math"
 )
 
-
-
 var (
-	apiKey = ""
+	apiKey    = ""
 	secretKey = ""
-
 )
 
 type Accuracy func() float64
@@ -39,44 +36,28 @@ func (this Accuracy) SmallerOrEqual(a, b float64) bool {
 	return math.Max(a, b) == b || math.Abs(a-b) < this()
 }
 
-
-
 var client = binance.NewClient(apiKey, secretKey)
 
-func sysmbol_price(sysmbol string) (price string){
-	prices, err := client.NewListPricesService().Do(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, pair := range prices{
-		if pair.Symbol == sysmbol{
-			return pair.Price
-		}
-	}
-	return ""
-}
-
-func get_balance(coin string) (qty float64) {
+func getBalance(symbol string) (qty float64) {
 	res, err := client.NewGetAccountService().Do(context.Background())
 	var count float64
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _,each := range res.Balances{
-		if each.Asset == coin{
-			count,_ = strconv.ParseFloat(each.Free,64)
+	for _, each := range res.Balances {
+		if each.Asset == symbol {
+			count, _ = strconv.ParseFloat(each.Free, 64)
 		}
 	}
 	return count
 
 }
 
-func trend() (state bool)  {
-	endTime := time.Now().UnixNano()/1e6
-	startTime := time.Now().Add(-time.Minute*60).UnixNano()/1e6
-	fmt.Println(endTime,startTime)
+func trend() (state bool) {
+	endTime := time.Now().UnixNano() / 1e6
+	startTime := time.Now().Add(-time.Minute*60).UnixNano() / 1e6
+	fmt.Println(endTime, startTime)
 	trades, err := client.NewAggTradesService().
 		Symbol("ETHBTC").StartTime(startTime).
 		EndTime(endTime).Do(context.Background())
@@ -84,35 +65,35 @@ func trend() (state bool)  {
 		fmt.Println(err)
 		return
 	}
-	var start_temp float64 = 0
+	var startTemp float64 = 0
 	for _, t := range trades[:10] {
-		t_Price,_ := strconv.ParseFloat(t.Price,64)
-		start_temp = t_Price + start_temp
+		tPrice, _ := strconv.ParseFloat(t.Price, 64)
+		startTemp = tPrice + startTemp
 	}
-	start_ave := start_temp/10
+	startAve := startTemp / 10
 
-	var end_temp float64 = 0
+	var endTemp float64 = 0
 	for _, t := range trades[len(trades)-10:] {
-		t_Price,_ := strconv.ParseFloat(t.Price,64)
-		end_temp = t_Price + end_temp
+		tPrice, _ := strconv.ParseFloat(t.Price, 64)
+		endTemp = tPrice + endTemp
 	}
-	end_ave := end_temp/10
-	fmt.Println(start_ave,end_ave)
+	endAve := endTemp / 10
+	fmt.Println(startAve, endAve)
 
 	var a Accuracy = func() float64 { return 0.0000001 }
 
-	if a.Greater(start_temp,end_ave){
+	if a.Greater(startTemp, endAve) {
 		return false
-	}else {
+	} else {
 		return true
 	}
 
 }
 
-func trend_small() (state int){
-	endTime := time.Now().UnixNano()/1e6
-	startTime := time.Now().Add(-time.Minute*10).UnixNano()/1e6
-	fmt.Println(endTime,startTime)
+func trendSmall() (state int) {
+	endTime := time.Now().UnixNano() / 1e6
+	startTime := time.Now().Add(-time.Minute*10).UnixNano() / 1e6
+	fmt.Println(endTime, startTime)
 	trades, err := client.NewAggTradesService().
 		Symbol("ETHBTC").StartTime(startTime).
 		EndTime(endTime).Do(context.Background())
@@ -120,30 +101,30 @@ func trend_small() (state int){
 		fmt.Println(err)
 		return
 	}
-	var start_temp float64 = 0
+	var startTemp float64 = 0
 	for _, t := range trades[:10] {
-		t_Price,_ := strconv.ParseFloat(t.Price,64)
-		start_temp = t_Price + start_temp
+		tPrice, _ := strconv.ParseFloat(t.Price, 64)
+		startTemp = tPrice + startTemp
 	}
-	start_ave := start_temp/10
+	startAve := startTemp / 10
 
 	var end_temp float64 = 0
 	for _, t := range trades[len(trades)-10:] {
-		t_Price,_ := strconv.ParseFloat(t.Price,64)
-		end_temp = t_Price + end_temp
+		tPrice, _ := strconv.ParseFloat(t.Price, 64)
+		end_temp = tPrice + end_temp
 	}
-	end_ave := end_temp/10
-	fmt.Println(start_ave,end_ave)
+	endAve := end_temp / 10
+	fmt.Println(startAve, endAve)
 
 	var a Accuracy = func() float64 { return 0.0000001 }
 
-	if a.Greater(start_ave,end_ave){
+	if a.Greater(startAve, endAve) {
 		fmt.Println(-1)
 		return -1
-	}else if (a.Equal(start_ave,end_ave)) {
+	} else if a.Equal(startAve, endAve) {
 		fmt.Println(0)
 		return 0
-	}else if(a.Smaller(start_ave,end_ave)){
+	} else if a.Smaller(startAve, endAve) {
 		fmt.Println(1)
 		return 1
 	} else {
@@ -151,31 +132,13 @@ func trend_small() (state int){
 	}
 }
 
-
 func Round2(f float64, n int) float64 {
 	floatStr := fmt.Sprintf("%."+strconv.Itoa(n)+"f", f)
 	inst, _ := strconv.ParseFloat(floatStr, 64)
 	return inst
 }
 
-func Max_min_arry(arr []float64) (float64,float64){
-	max := arr[0]
-	min := arr[0]
-	var a Accuracy = func() float64 { return 0.0000001 }
-
-	for _, t := range arr{
-		if a.Greater(t,max) {
-			max = t
-		}
-		if a.Smaller(t,min){
-			min = t
-		}
-	}
-	return max, min
-	}
-
-
-func limit_order(sell_per float64, buy_per float64)  {
+func limitOrder(sellPer float64, buyPer float64) {
 	res, err := client.NewDepthService().Symbol("ETHBTC").
 		Do(context.Background())
 
@@ -183,32 +146,28 @@ func limit_order(sell_per float64, buy_per float64)  {
 		fmt.Println(err)
 		return
 	}
-	//market_price := sysmbol_price("ETHBTC")
-	buy_price := res.Bids[0].Price
-	sell_price := res.Bids[0].Price
+	// 买一 卖一价
+	buyPrice := res.Bids[0].Price
+	sellPrice := res.Bids[0].Price
 
-	f_sell_price,_ := strconv.ParseFloat(sell_price,64)
-	fmt.Println(f_sell_price)
-	f_sell_price = Round2(f_sell_price*sell_per,6)
-	f_buy_price,_ := strconv.ParseFloat(buy_price,64)
-	fmt.Println(f_buy_price)
-	f_buy_price = Round2(f_buy_price*buy_per,6)
+	fSellPrice, _ := strconv.ParseFloat(sellPrice, 64)
+	fmt.Println(fSellPrice)
+	fSellPrice = Round2(fSellPrice*sellPer, 6)
+	fBuyPrice, _ := strconv.ParseFloat(buyPrice, 64)
+	fmt.Println(fBuyPrice)
+	fBuyPrice = Round2(fBuyPrice*buyPer, 6)
 
-	s_sell_price := strconv.FormatFloat(f_sell_price,'g',-1,64)
-	s_buy_price := strconv.FormatFloat(f_buy_price,'g',-1,64)
-	fmt.Println(s_sell_price,s_buy_price)
+	sSellPrice := strconv.FormatFloat(fSellPrice, 'g', -1, 64)
+	sBuyPrice := strconv.FormatFloat(fBuyPrice, 'g', -1, 64)
+	fmt.Println(sSellPrice, sBuyPrice)
 
-	TradeSize  := get_balance("ETH") *0.98
-	TradeSize_count := Round2(TradeSize,3)
-	s_tradesize := strconv.FormatFloat(TradeSize_count,'g',-1,64)
-	fmt.Println(s_tradesize)
-
-
-
-
+	tradeSize := getBalance("ETH") * 0.98
+	tradeSizeCount := Round2(tradeSize, 3)
+	sTradeSize := strconv.FormatFloat(tradeSizeCount, 'g', -1, 64)
+	fmt.Println(sTradeSize)
 
 	//sell_trade_size := strconv.FormatFloat(Tradesize,'g',-1,64)
-	if ((f_sell_price-f_buy_price)>0) && (f_buy_price < 0.00031){
+	if ((fSellPrice - fBuyPrice) > 0) && (fBuyPrice < 0.00031) {
 
 		//endTime := time.Now().UnixNano()/1e6
 		//startTime := time.Now().Add(-time.Hour*1).UnixNano()/1e6
@@ -228,22 +187,22 @@ func limit_order(sell_per float64, buy_per float64)  {
 		//max_price , min_price := Max_min_arry(prices)
 		//var a Accuracy = func() float64 { return 0.0000001 }
 		//if (a.Greater(max_price*1.008,f_sell_price)) && (a.Smaller(min_price*0.992,f_buy_price)){
-			_, err1 := client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeSell).Type(binance.OrderTypeLimit).TimeInForce(binance.TimeInForceGTC).Quantity(s_tradesize).Price(s_sell_price).Do(context.Background())
-			if err1 != nil {
-				fmt.Println(err1)
-				return
-			}
+		_, err1 := client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeSell).Type(binance.OrderTypeLimit).TimeInForce(binance.TimeInForceGTC).Quantity(sTradeSize).Price(sSellPrice).Do(context.Background())
+		if err1 != nil {
+			fmt.Println(err1)
+			return
+		}
 
-			_, err2 := client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeBuy).Type(binance.OrderTypeLimit).TimeInForce(binance.TimeInForceGTC).Quantity(s_tradesize).Price(s_buy_price).Do(context.Background())
-			if err2 != nil {
-				fmt.Println(err2)
-				return
-			}
+		_, err2 := client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeBuy).Type(binance.OrderTypeLimit).TimeInForce(binance.TimeInForceGTC).Quantity(sTradeSize).Price(sBuyPrice).Do(context.Background())
+		if err2 != nil {
+			fmt.Println(err2)
+			return
+		}
 		//}
 	}
 }
 
-func order_state()  (state bool){
+func orderState() (state bool) {
 	orders, err := client.NewListOpenOrdersService().Symbol("ETHBTC").
 		Do(context.Background())
 	if err != nil {
@@ -257,50 +216,45 @@ func order_state()  (state bool){
 	return false
 }
 
-
-
-
-
-func main()  {
-	var counts int = 0
+func main() {
+	counts := 0
 	for {
-		if order_state() == false{
-			counts =0
-			time.Sleep(time.Minute*60*5)
+		if !orderState() {
+			counts = 0
+			time.Sleep(time.Hour * 5)
 
-			if trend() == false {
-				limit_order(1.001, 0.995)
+			if !trend() {
+				limitOrder(1.001, 0.995)
 			}
 			//} else {
 			//	limit_order(1.010,1)
 			//}
 		}
-		counts ++
-		time.Sleep(time.Minute*1)
-		if (counts >5440){
+		counts++
+		time.Sleep(time.Minute * 1)
+		if counts > 5440 {
 			orders, _ := client.NewListOpenOrdersService().Symbol("ETHBTC").
 				Do(context.Background())
-			for _, o := range orders{
-				Order_id := o.OrderID
-				Order_Side := o.Side
-				Tradesize := o.OrigQuantity
-				if (Order_Side == "SELL") {
-					if (trend_small() == -1) {
+			for _, o := range orders {
+				orderId := o.OrderID
+				orderSide := o.Side
+				tradeSize := o.OrigQuantity
+				if orderSide == "SELL" {
+					if trendSmall() == -1 {
 						_, err := client.NewCancelOrderService().Symbol("ETHBTC").
-							OrderID(Order_id).Do(context.Background())
+							OrderID(orderId).Do(context.Background())
 						if err == nil {
 							client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeSell).
-								Type(binance.OrderTypeMarket).Quantity(Tradesize).Do(context.Background())
+								Type(binance.OrderTypeMarket).Quantity(tradeSize).Do(context.Background())
 						}
 					}
-				} else if (Order_Side == "BUY") {
-
-					if (trend_small() == 1) {
+				} else if tradeSize == "BUY" {
+					if trendSmall() == 1 {
 						_, err := client.NewCancelOrderService().Symbol("ETHBTC").
-							OrderID(Order_id).Do(context.Background())
+							OrderID(orderId).Do(context.Background())
 						if err == nil {
 							client.NewCreateOrderService().Symbol("ETHBTC").Side(binance.SideTypeBuy).
-								Type(binance.OrderTypeMarket).Quantity(Tradesize).Do(context.Background())
+								Type(binance.OrderTypeMarket).Quantity(tradeSize).Do(context.Background())
 						}
 					}
 				}
